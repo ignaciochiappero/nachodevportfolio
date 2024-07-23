15d158cb259fdf6f2b6fcf9e408ddc8a149739c1
nachochiappero <ignaciogabrielchiappero@gmail.com>
2024-07-23 12:53:58 -0300

    agregado imagenes 3 primeros proyectos en portolio

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -1,8 +1,4 @@
-
-
 import { dataPortfolio } from "@/data";
-
-
 
 import TransitionPage from "@/components/transition-page";
 import ContainerPage from "@/components/container-page";
@@ -10,28 +6,28 @@ import PortfolioBox from "@/components/portfolio-box";
 import CoverParticles from "@/components/cover-particles";
 
 const PortfolioPage = () => {
+  return (
+    <>
+      <TransitionPage />
+      <CoverParticles />
+      <ContainerPage>
+        <div className="flex flex-col justify-center h-full">
+          <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">
+            Mis últimos{" "}
+            <span className="font-bold text-secondary">
+              trabajos realizados
+            </span>
+          </h1>
 
-    return (
-        <>
-         <TransitionPage />
-         <CoverParticles/>
-        <ContainerPage>
-            
-           
-            
-            
-            <div className="flex flex-col justify-center h-full">
-                <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">Mis últimos <span className="font-bold text-secondary">trabajos realizados</span></h1>
-
-                <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-4">
-                    {dataPortfolio.map((data) => (
-                        <PortfolioBox key={data.id} data={data} />
-                    ))}
-                </div>
-            </div>
-        </ContainerPage>
-        </>
-    );
-}
+          <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-3 lg:grid-cols-4">
+            {dataPortfolio.map((data) => (
+              <PortfolioBox key={data.id} data={data} />
+            ))}
+          </div>
+        </div>
+      </ContainerPage>
+    </>
+  );
+};
 
 export default PortfolioPage;
