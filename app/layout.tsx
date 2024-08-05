844c2771f3fd946a40ba3db27c865eab60cf60a4
nachochiappero <ignaciogabrielchiappero@gmail.com>
2024-08-04 21:43:49 -0300

    mejora en el seo de google

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 import Navbar from "@/components/navbar";
-
 import Header from "@/components/header";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
@@ -17,7 +16,30 @@ const urbanist = Urbanist({ subsets: ["latin"] });
 //Title and description 
 export const metadata: Metadata = {
   title: "Nacho Dev",
-  description: "Page made by Nacho Chiappero",
+  description: "Soy Nacho Chiappero, desarrollador web Full Stack, desarrollador de AI y diseñador gráfico. Explora mi portfolio para ver mis proyectos y contactarme.",
+  openGraph: {
+    title: "Nacho Dev",
+    description: "Soy Nacho Chiappero, desarrollador web Full Stack, desarrollador de AI  y diseñador gráfico. Explora mi portfolio para ver mis proyectos y contactarme.",
+    images: [
+      {
+        url: "/Logo2.png",
+        alt: "Logo de Nacho Dev",
+        width: 1200,
+        height: 630
+      }
+    ],
+    siteName: "Nacho Dev",
+    locale: "en_US",
+    type: "website"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Nacho Dev",
+    description: "Soy Nacho Chiappero, desarrollador web Full Stack, desarrollador de AI  y diseñador gráfico. Explora mi portfolio para ver mis proyectos y contactarme.",
+    images: ["/Logo2.png"],
+    creator: "@nachodev"
+  },
+  metadataBase: new URL("https://www.nachochiapperodev.com"), 
 };
 
 export default function RootLayout({
@@ -28,13 +50,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={urbanist.className}> 
-        
         <Navbar />
-        
         <Header />
         {children}
-        
-        </body>
+      </body>
     </html>
   );
 }
